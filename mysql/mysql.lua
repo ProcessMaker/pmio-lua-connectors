d5ae7c84e8f1cf700bf486d985ef39adf9fe9ae0
Svetlana Khorolets <svkhorolets@gmail.com>
2018-03-12 14:40:49 +0200

    Add mysql connector (#8)
    * Add mysql connector
    
    * Result of mysql query now returns in an array

---
+++ mysql/mysql.lua
@@ -0,0 +1,66 @@
+--[[
+# MySQL connector
+
+## Requirements:
+
+  * lua-cjson
+  * [luasql-mysql](https://keplerproject.github.io/luasql/index.html)
+
+  ```
+  sudo luarocks install luasql-mysql
+  sudo luarocks install lua-cjson
+  ```
+
+## Input parameters
+
+  * database - name of database to connect
+  * username - username to MySQL server
+  * password - password for username
+  * host - host of MySQL (default: 'localhost')
+  * port - port of MySQL (default: 3306)
+  * sqlQuery - SQL query to execute, e.g. `SELECT name, email from people`
+
+
+## Output parameters
+
+  Example response:
+
+  ```
+  [
+    {"name":"Jose das Couves","email":"jose@couves.com"}
+    {"name":"Manoel Joaquim","email":"manoel.joaquim@cafundo.com"}
+    {"name":"Maria das Dores","email":"maria@dores.com"}
+  ]
+  ```
+]]--
+local cjson = require("cjson")
+-- load driver
+local driver = require "luasql.mysql"
+
+local inputVar = cjson.decode(io.stdin:read("*a"))
+
+local database = inputVar.database
+local username = inputVar.username
+local password = inputVar.password
+local host = inputVar.host or 'localhost'
+local port = inputVar.port or 3306
+local sqlQuery = inputVar.sql_query
+
+-- create environment object
+env = assert (driver.mysql())
+-- connect to data source
+con = assert (env:connect(database, username, password, host, port))
+-- retrieve a cursor
+cur = assert (con:execute(sqlQuery))
+-- print all rows
+local rows = {}
+row = cur:fetch ({}, "a")
+while row do
+  rows[#rows+1] = row
+  row = cur:fetch ({}, "a")
+end
+print(cjson.encode(rows))
+-- close everything
+cur:close() -- already closed because all the result set was consumed
+con:close()
+env:close()
